82e7e7f23f3f1b2753cca79d35f07341e2a7e8dd
Ephraim Valladares <effelion@gmail.com>
2024-10-01 16:25:47 +0200

    fixed repeated code

--- spel.py
+++ spel.py
@@ -5,10 +5,7 @@ player=""
 computer=""
 result=""
 repeat= True
-def random_choices ():
-    hands = [figures.rock, figures.paper, figures.scissors]
-    selected_hand = random.choice(hands)
-    return selected_hand
+
 
 class Winner:
     def __init__(self,name):
@@ -16,6 +13,12 @@ class Winner:
 
     def __str__ (self):
         return f"the winner is {self.name} "
+
+
+def random_choices ():
+    hands = [figures.rock, figures.paper, figures.scissors]
+    selected_hand = random.choice(hands)
+    return selected_hand
 
 start = input("vill du köra Sten , Sax, Påse ? Y eller N \n")
 if start.lower() == "y":
@@ -27,26 +30,21 @@ if start.lower() == "y":
         print("Computer :")
         print(computer)
 
-        if player == figures.paper and computer == figures.scissors:
-            result= Winner("computer")
-            print(result)
-        elif player == figures.paper and computer == figures.rock:
+
+        if player == figures.paper and computer == figures.rock:
             result= Winner("Player 1")
             print(result)
         elif player == figures.rock and computer == figures.scissors:
             result=Winner("Player 1")
             print(result)
-        elif player == figures.rock and computer == figures.paper:
-             result = Winner("Computer")
-             print(result)
         elif player == figures.scissors and computer == figures.paper:
             result=Winner("Player 1")
             print(result)
-        elif player == figures.scissors and computer == figures.rock:
-            result=Winner("Computer")
-            print(result)
+        elif player == computer:
+            print("Its a Draw")
         else:
-            print("its a draw")
+            result= Winner("Computer")
+            print(result)
         again = input("continue playing ? Y or N \n")
         if again.lower() == "n":
             repeat= False
